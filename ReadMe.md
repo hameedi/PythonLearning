d8b750333fd0eaa8bbbe57c9992e725993f5e256
MFurqanHameedi <mohammad_furqan@hotmail.com>
2020-01-09 19:41:07 +0500

    05 Module : Modularity Completed.

---
+++ ReadMe.md
@@ -0,0 +1,3 @@
+
+PluralSight Course : Core Python getting Started 
+Url : https://app.pluralsight.com/library/courses/getting-started-python-core/table-of-contents
--- words.py
+++ words.py
@@ -1,12 +1,35 @@
+import sys 
 from urllib.request import urlopen 
 
-story = urlopen('http://sixty-north.com/c/t.txt')
-story_words = []
-for line in story:
-    line_words = line.decode('utf').split()
-    for word in line_words:
-        story_words.append(word)
-story.close()
 
-for word in story_words:
-    print(word)
+def fetch_words(url):
+    # ***Fetch a list of words from a Url.
+    # Args: 
+    #     url: The URL of a UTF-8 Document 
+
+    # Reurns:
+    #     A list of strings containing the words from 
+    #     the document.
+    # ***
+    story = urlopen(url)
+    story_words = []
+    for line in story:
+        line_words = line.decode('utf').split()
+        for word in line_words:
+            story_words.append(word)
+    story.close()
+    return story_words
+
+
+def print_items(items):
+    for item in items:
+        print(item)
+
+
+def main(url):
+    words = fetch_words(url)
+    print_items(words)
+
+
+if __name__ == '__main__':
+    main(sys.argv[1])
